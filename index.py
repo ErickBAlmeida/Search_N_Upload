92045fd1f6c6a6e140a2607d137a77a9da346a05
Erick de Almeida <hxcxalmeida@gmail.com>
2025-09-25 17:10:52 -0300

    :sparkles: Novas funcionalidade de relatórios e tratamento de erros

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 import os
+import time
 from pathlib import Path
 
 import requests
@@ -12,7 +13,7 @@ class Planilha:
         self.diretorios = diretorios
         self.API = api
 
-        caminho_honorarias = Path(os.getenv("PARCELAS_HONORARIAS"))
+        caminho_honorarias = Path(os.getenv("CONFISSOES"))
         self.wb = load_workbook(caminho_honorarias)
         self.sheet_honorarias = self.wb.active
 
@@ -22,34 +23,76 @@ class Planilha:
 
     def iterar_base(self):
         for linha in range (2, self.sheet_honorarias.max_row+1):
-            if self.sheet_honorarias.cell(row=linha, column=1).value is None:
+            time.sleep(.5)
+            nome = self.sheet_honorarias.cell(row=linha, column=1).value
+            if nome is None:
                 break
             
             processo = self.sheet_honorarias.cell(row=linha, column=2).value
-            reu = self.buscar_nome_do_reu(processo)      
 
-            if reu == None:
-                print(f"\n❌ Não foi possível localizar o processo: {processo} na planilha 3C.")
-                continue
-            
-            else:
+            try:
+                print("\n============================================================\n")
+                print(f"Buscando os dados do(a) {nome}.....")
+                caminho, arquivo = self.diretorios.pegar_arquivo(nome)
+                novo_caminho, novo_arquivo = self.diretorios.renomear_arquivo(processo, caminho, arquivo)
+                self.API.upload(processo, novo_caminho, novo_arquivo)
+
+            except:
                 try:
-                    caminho, arquivo = self.diretorios.pegar_arquivo(reu)
-                    novo_caminho, novo_arquivo = self.diretorios.renomear_arquivo(processo, caminho, arquivo)
-                    self.API.upload(processo, novo_caminho, novo_arquivo)
+                    print("Buscando o dados no 3C...")
+                    nome_alternativo = self.buscar_nome_na_base_3C(processo)
+                    if nome_alternativo is not None:
+                        caminho, arquivo = self.diretorios.pegar_arquivo(nome_alternativo)
 
+                        if caminho is None:
+                            print(f"\n❌ O processo '{processo}' não possui um diretório")
+                            self.criar_relatorio(nome, processo, "DIRETÓRIO INEXISTENTE")
+                            continue
+                        elif arquivo is None:
+                            print(f"\n❌ O processo '{processo}' não possui um documento de confissão")
+                            self.criar_relatorio(nome, processo, "NÃO POSSUI CONFISSÃO")
+                            continue
+                        novo_caminho, novo_arquivo = self.diretorios.renomear_arquivo(processo, caminho, arquivo)
+                        
+                        response = self.API.upload(processo, novo_caminho, novo_arquivo)
+                        match response:
+                            case 1:
+                                ...
+                            case 2:
+                                self.criar_relatorio(nome, processo, "DIRETÓRIO INEXISTENTE")
+                            case 3:
+                                self.criar_relatorio(nome, processo, "PROCESSO NÃO LOCALIZADO NO SISTEMA")
+                            case 4 :
+                                self.criar_relatorio(nome, processo, "ERRO DE API")
+                    else:
+                        raise
                 except:
-                    print(f"\n❌ Não foi possível localizar arquivo do réu: {reu}")
-                
-    def buscar_nome_do_reu(self, processo):
+                    print(f"\n❌ O nome: {nome} não foi localizado em lugar algum")
+                    self.criar_relatorio(nome, processo, "NOME NÃO LOCALIZADO")
+                                    
+    def buscar_nome_na_base_3C(self, processo):
         for linha in range (2, self.sheet_3c.max_row+1):
             temp_processo = self.sheet_3c.cell(row=linha, column=2).value
 
             if str(temp_processo) == str(processo):
                 nome_do_reu = self.sheet_3c.cell(row=linha, column=1).value
                 return nome_do_reu            
-            
         return None
+
+    def criar_relatorio(self, nome, processo, razao):
+        relatorio = [nome, processo, razao]
+        self.wb = load_workbook("planilhas\RELATORIO.xlsx")
+        sheet = self.wb.active
+
+        try:
+            sheet.append(relatorio)
+            print(f"✅ Relatório de '{nome}' pre-armazenados com sucesso.")
+        
+        except Exception as e:
+            print(f"❌ Erro ao armazenar relatório.             Detalhers do erro: {e}")
+    
+    def salvar_relatorio(self):
+        self.wb.save("planilhas\RELATORIO.xlsx")
 
 class Diretorios:
     def __init__(self):
@@ -62,22 +105,21 @@ class Diretorios:
                 if "CONFISSÃO" in elemento.name.upper() or "CONFISSAO" in elemento.name.upper():
                     print(f"\n✅ Arquivo do réu: '{nome_do_reu}' localizado!!")
                     return elemento.parent, elemento.name
-            
-            return None
-        return None
+            return diretorio_do_reu, None
+        return None, None
         
     def renomear_arquivo(self, processo, caminho, arquivo):
+        novo_nome = f"{processo}_CONFISSAO_DE_DIVIDA"
         caminho_original = os.path.join(caminho, arquivo)
-        novo_nome = f"{processo}_{arquivo}".replace(' ', '_')
         novo_caminho = os.path.join(caminho, novo_nome)
 
         try:
             Path(caminho_original).rename(novo_caminho)
             print(f"✅ CONFISSÃO RENOMEADA PARA -> {novo_caminho}")
             return novo_caminho, novo_nome
-        
+
         except Exception as e:
-            print(f"\n❌ Não foi possível renomear a o arquivo CONFISSÃO.\nDetalhes do erro:{e}")
+            print(f"\n❌ Não foi possível renomear o arquivo CONFISSÃO.\nDetalhes do erro: {e}")
 
 class API:
     def __init__(self):
@@ -91,31 +133,35 @@ class API:
         }
     
     def upload(self, processo, caminho, arquivo):        
-        if not os.path.exists(caminho):
-            print(f"Arquivo não encontrado: {caminho}")
-            return
-
-        print(f"🟠 Enviando {arquivo} para o processos {processo} ...")
+        file_path = Path(caminho)
+        if not file_path.exists():
+            print(f"❌ Arquivo não encontrado: {file_path}")
+            return 0
         
-        with open(Path(caminho), "rb") as f:
+        print(f"🟠 Enviando {arquivo} para o processos {processo} ...")
+        with open(file_path, "rb") as f:
             files = {
-                "document" : (arquivo, f, "application/pdf")
+                "document" : (file_path.name, f, "application/pdf")
             }
-            response = requests.post(self.URL, files=files, headers=self.headers)
-
-        match response.status_code:
-            case 200 | 202:
-                print(f"✅ Upload realizado com sucesso.             Status: {response.status_code}\n")
+            response = requests.post(self.URL, files=files, headers=self.headers, timeout=10)
+            match response.status_code:
+                case 200:
+                    print(f"✅ Upload realizado com sucesso.             Status: {response.status_code}\n")
+                    return 1
+                case 400:
+                    print(f"❌ Arquivo não encontrado: {caminho}.             Status: {response.status_code}")
+                    return 2
+                case 404:
+                    print(f"❌ Processos {processo} não foi encontrado no sistema.             Status: {response.status_code}")
+                    return 3
+                case _ :
+                    print(f"❌ Erro de upload desconhecido: \n\nprocesso: {processo}\ncaminho do arquivo: {os.path.join(caminho or '', arquivo or '')}\nStatus: {response}")
+                    return 4
             
-            case 400 | 404:
-                print(f"❌ Processos {processo} não foi encontrado no sistema.             Status: {response.status_code}")
-            
-            case _ :
-                print(f"❌ Erro de upload desconhecido: \n\nprocesso: {processo}\ncaminho do arquivo: {os.path.join(caminho, arquivo)}\nStatus: {response.status_code}")
-
 if __name__ == "__main__":
     diretorios = Diretorios()
     api = API()
     
     base = Planilha(diretorios, api)
     base.iterar_base()
+    base.salvar_relatorio() #BUG -> Criou apenas o relatório do último caso
